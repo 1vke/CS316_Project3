a3fb65cc20fdc4ab7ccb085ebffc9b211f283b22
Lucas Lowe <lowel9295@gmail.com>
2025-09-26 11:33:25 -0400

    Improve client logic

--- src/FileClient.java
+++ src/FileClient.java
@@ -1,7 +1,14 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class FileClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 2) {
             System.out.println("Need server IP and server port");
         }
@@ -10,26 +17,38 @@ public class FileClient {
         int serverPort = Integer.parseInt(args[1]);
 
         Scanner scnr = new Scanner(System.in);
-        String message;
+        List<String> commandArgs = new ArrayList<>();
+        String command = "";
 
         do {
-            message = scnr.nextLine().toLowerCase();
+            try (SocketChannel channel = SocketChannel.open(new InetSocketAddress(serverIP, serverPort))) {
+                commandArgs = Arrays.stream(scnr.nextLine().split(" ")).toList();
+                command = commandArgs.getFirst();
 
-            switch (message) {
-                case "list":
-                    break;
-                case "delete":
-                    break;
-                case "rename":
-                    break;
-                case "download":
-                    break;
-                case "upload":
-                    break;
-                default:
-                    System.out.println("Invalid command");
+//                ByteBuffer buffer = ByteBuffer.wrap(messageBytes);
+//                buffer.put(messageBytes);
+//                buffer.flip();
+//
+//                channel.write(buffer);
+//                channel.shutdownOutput();
+
+                switch (command) {
+                    case "list":
+                        break;
+                    case "delete":
+                        break;
+                    case "rename":
+                        break;
+                    case "download":
+                        break;
+                    case "upload":
+                        break;
+                    default:
+                        System.out.println("Invalid command");
+                }
+            } catch (Exception e) {
+                System.out.printf("There was an error! Try again. \n %s\n", e.getMessage());
             }
-
-        } while (!message.equals("quit"));
+        } while (!command.equalsIgnoreCase("quit"));
     }
 }
