e124a8fea861b5b2cd91b879b78b6ba88f2af82a
Lucas Lowe <lowel9295@gmail.com>
2025-09-26 11:40:58 -0400

    Update server

--- src/FileServer.java
+++ src/FileServer.java
@@ -1,2 +1,20 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
 public class FileServer {
+    public static void main(String[] args) {
+        int port = 3000;
+
+        try (ServerSocketChannel channel = ServerSocketChannel.open()) {
+            channel.bind(new InetSocketAddress(port));
+
+            while (true) {
+                SocketChannel socketChannel = channel.accept();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
